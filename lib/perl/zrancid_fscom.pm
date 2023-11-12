52f68b583fbc893753c678c08271197acc8a8d22
Julien Thomas <jthomas@zenetys.com>
2023-11-12 13:03:50 +0100

    FEATURE: fscom: Add rancid type z-fscom

---
+++ lib/perl/zrancid_fscom.pm
@@ -0,0 +1,16 @@
+package zrancid_fscom;
+
+sub import { return 0; }
+sub init { return 0; }
+
+$zrancid::options{config_end_on_prompt} = 1;
+$zrancid::options{commands}{'show version'}{prefix} = '!Version: ';
+$zrancid::options{commands}{'show version'}{subs} = [
+    [ '( uptime is )[^,]+(, The current time: ).*', '$1<removed>$2<removed>' ],
+    [ '(System uptime\s+: ).*', '$1<removed>' ],
+];
+$zrancid::options{commands}{'show running-config'}{subs} = [
+    [ '^(username [^ ]+ [^ ]+ [^ ]+ ).*', '!$1<removed>', ],
+];
+
+return 1;
