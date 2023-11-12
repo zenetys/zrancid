53c7878cf52283addfdc5cd66179c044d4e65d40
Julien Thomas <jthomas@zenetys.com>
2023-11-12 12:23:41 +0100

    MINOR: cisco-ucs: Substitute lines with a date changing too often

--- lib/perl/zrancid_cisco_ucs.pm
+++ lib/perl/zrancid_cisco_ucs.pm
@@ -6,8 +6,11 @@ sub init { return 0; }
 $zrancid::options{commands}{'show version brief'}{prefix} = '!Version: ';
 $zrancid::options{commands}{'show configuration all | no-more'}{subs} = [
     [ '  (           set date )\d\d? 202\d \d\d? \d\d? \d\d?$', ' !$1<removed>' ],
-    [ '(^\s*!\s*enter backup .*)(20\d\d-\d\d-\d\dT\d\d-\d\d-\d\d\.\d\d\d)(.*)', '$1<removed>$3' ],
+    [ '(^\s*!\s*enter backup .*)(20\d\d-\d\d-\d\dT\d\d-\d\d-\d\d\.\d\d\d)(.*)(enabled|disabled)(.*)', '$1<removed>$3<removed>$5' ],
+    [ '(^\s*!\s*enter backup .*[^0-9])(\d{10,})(.*)(enabled|disabled)(.*)', '$1<removed>$3<removed>$5' ],
+    [ '(^\s*!\s*enter backup .* )(enabled|disabled)(.*)', '$1<removed>$3' ],
     [ '(^\s*!\s*set remote-file .*)(20\d\d-\d\d-\d\dT\d\d-\d\d-\d\d\.\d\d\d)(.*)', '$1<removed>$3' ],
+    [ '(^\s*!\s*set remote-file .*[^0-9])(\d{10,})(.*)', '$1<removed>$3' ],
 ];
 
 return 1;
