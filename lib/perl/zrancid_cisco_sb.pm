5891b3c06d0217ba6d42fcc0a8069bbaa2098b3e
Julien Thomas <jthomas@zenetys.com>
2024-01-04 18:02:53 +0100

    MINOR: cisco-sb: Drop escape sequence seen on CBS250-8T-D

--- lib/perl/zrancid_cisco_sb.pm
+++ lib/perl/zrancid_cisco_sb.pm
@@ -9,6 +9,7 @@ $zrancid::options{inloop_pre_hook} = sub {
     $fd_in = zrancid::_modraw($fd_in, sub {
         my $line = $_;
         $line =~ s/\x1b\[A//g; # Sx220
+        $line =~ s/\x1b\[0m//g; # CBS250-8T-D
         return undef if (/^\S+ uptime is [0-9]+/); # Sx220
         return undef if (/^! System Up Time: [0-9]+/); # Sx220, Planet GS421024T2S
         return undef if (/^SYSTEM CONFIG FILE ::= BEGIN/); # Planet GS421024T2S
