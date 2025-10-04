1fd298858440cb0a4ccdc412b5b628c9b62a8b17
Julien Thomas <jthomas@zenetys.com>
2025-10-04 17:54:05 +0200

    MINOR: fscom: System start time second accuracy causing too much flapping
    Example:
    
    - !Version: System start time       : 2025-09-19 11:30:03
    + !Version: System start time       : 2025-09-19 11:30:02

--- lib/perl/zrancid_fscom.pm
+++ lib/perl/zrancid_fscom.pm
@@ -7,6 +7,7 @@ $zrancid::options{config_end_on_prompt} = 1;
 $zrancid::options{commands}{'show version'}{prefix} = '!Version: ';
 $zrancid::options{commands}{'show version'}{subs} = [
     [ '( uptime is )[^,]+(, The current time: ).*', '$1<removed>$2<removed>' ],
+    [ '(System start time\s+: ).*', '$1<removed>' ],
     [ '(System uptime\s+: ).*', '$1<removed>' ],
 ];
 $zrancid::options{commands}{'show running-config'}{subs} = [
